313cdc1638060659854ec7976bedac57ce98b339
Michal Oleszczyk <m.oleszczyk@grinn-global.com>
2017-10-24 14:45:00 +0200

    Add 2G-board example for chiliboard.

---
+++ board/grinn/chiliboard-2g/post-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# To use correct device-tree blob we have two options:
+#
+# 1. correct name of DTB file in the U-Boot, correct genimage file, etc.
+# 2. replace imx6ul-liteboard.dtb file by this customized by this project; while
+#    name of the file is this same U-Boot will be happy and our customized device-tree
+#    will be also forwarded to device image.
+cp ${BINARIES_DIR}/am335x-chiliboard-2g.dtb ${BINARIES_DIR}/am335x-chiliboard.dtb
